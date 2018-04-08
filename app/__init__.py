100d7f0a9dbfc4194fc72b7aec7ef452b0ac6eb9
Carina Sauter <carina.sauter@berkeley.edu>
2018-04-08 01:57:30 -0700

    login and signup frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_material import Material
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+# Material(app)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Required
 from wtforms.fields.html5 import EmailField
 from .models import *
-
 
 # class TripForm(Form):
 # 	tripname = StringField('tripname', validators=[DataRequired()])
@@ -20,7 +19,6 @@ class LoginForm(Form):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember Me')
-	submit = SubmitField('Log In')
 
 
 class SignUpForm(Form):
