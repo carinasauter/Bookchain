939d74122cd2d27b226b0353436512ae45642ddd
Carina Sauter <carina.sauter@berkeley.edu>
2018-04-18 01:32:51 -0700

    deleted test cases

--- test/test_first.py
+++ test/test_first.py
@@ -5,13 +5,3 @@ from app.models import *
 def test_pytest():
     assert True == True
 
-
-def test_getUserbyID():
-	user = getUserByID(1)
-	assert "carina" == user.username
-
-
-def test_readingBooks():
-	user = getUserByID(1)
-	assert [] == user.readingBooks()
-
