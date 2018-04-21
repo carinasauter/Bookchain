98f44da177b7ba6b3d5bd861754b2b03856add16
Carina Sauter <carina.sauter@berkeley.edu>
2018-04-21 12:43:51 -0700

    first testcase

--- test/test_first.py
+++ test/test_first.py
@@ -2,6 +2,17 @@ import unittest
 from app.models import *
 
 
-def test_pytest():
-    assert True == True
+def test_createUser():
+	username = "testuser"
+	email = "test@test.com"
+	password_hash = ""
+	full_name = "Test User"
+	street = "2822 Garber Street"
+	city = "Berkeley"
+	state = "CA"
+	country = 'USA'
+	zipcode = "94705"
+	testUser = User(username, email, password_hash, full_name, street, city, state, country, zipcode)
+	assert username == testUser.username and email == testUser.email and password_hash == testUser.password_hash
+
 
