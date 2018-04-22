6d0a5156a02140a8a2b3334a30c2c579a823e127
Carina Sauter <carina.sauter@berkeley.edu>
2018-04-21 19:38:16 -0700

    2nd test case

--- test/test_first.py
+++ test/test_first.py
@@ -1,18 +1,34 @@
 import unittest
 from app.models import *
 
+username = "testuser"
+email = "test@test.com"
+password_hash = ""
+full_name = "Test User"
+street = "2822 Garber Street"
+city = "Berkeley"
+state = "CA"
+country = 'USA'
+zipcode = "94705"
+testUser = User(username, email, password_hash, full_name, street, city, state, country, zipcode)
 
 def test_createUser():
-	username = "testuser"
-	email = "test@test.com"
-	password_hash = ""
-	full_name = "Test User"
-	street = "2822 Garber Street"
-	city = "Berkeley"
-	state = "CA"
-	country = 'USA'
-	zipcode = "94705"
-	testUser = User(username, email, password_hash, full_name, street, city, state, country, zipcode)
-	assert username == testUser.username and email == testUser.email and password_hash == testUser.password_hash
+	assert username == testUser.username and email == testUser.email and \
+	password_hash == testUser.password_hash and full_name == testUser.full_name and \
+	street == testUser.street and city == testUser.city and state == testUser.state and \
+	country == testUser.country and zipcode == testUser.zipcode
 
 
+def test_addToDatabase():
+	testUser.addToDatabase()
+	with sql.connect('database.db') as connection:
+		cursor = connection.cursor()
+		check = cursor.execute("SELECT username from users order by user_id desc limit 1").fetchall()
+	assert check[0][0] == username
+
+
+def test_cleanup():
+	with sql.connect('database.db') as connection:
+		cursor = connection.cursor()
+		cursor.execute("DELETE FROM users WHERE username = ?", (username,))
+		connection.commit()
