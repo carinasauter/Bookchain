a50adec5e6e591218869f7ee8e145b3b8847e41f
Jing Xiong <jxiong@berkeley.edu>
2018-04-26 19:40:54 -0700

    limit length of state input to 2 characters

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm as Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, IntegerField, FormField
-from wtforms.validators import DataRequired, Required
+from wtforms.validators import DataRequired, Required, Length
 from wtforms.fields.html5 import EmailField
 from .models import *
 from flask_material import Material 
@@ -18,7 +18,7 @@ class SignUpForm(Form):
 	full_name = StringField('Full Name', validators=[DataRequired()])
 	street = StringField('Street', validators=[DataRequired()])
 	city = StringField('City', validators=[DataRequired()])
-	state = StringField('State', validators=[DataRequired()])
+	state = StringField('State', validators=[DataRequired(), Length(min=2, max=2, message="Please enter a two character abbreviation for the state you live in.")])
 	# we currently only accept users in U.S, so disable this field
 	# country = StringField('Country', validators=[DataRequired()])
 	zipcode = IntegerField('Zipcode', validators=[DataRequired()])
